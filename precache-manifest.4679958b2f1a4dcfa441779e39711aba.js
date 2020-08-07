d99264db7fd27020a9cc9c0e5ce7a3d6f68a7110
arpana23 <sinhaarpana.2019@gmail.com>
2020-08-07 22:47:26 +0530

    Updates

---
+++ precache-manifest.4679958b2f1a4dcfa441779e39711aba.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db6fd0ea14a43f24773de33d6728a77",
+    "url": "/inter-pro/index.html"
+  },
+  {
+    "revision": "2fe40c6aab871a03e69b",
+    "url": "/inter-pro/static/css/2.1cf7c4bb.chunk.css"
+  },
+  {
+    "revision": "cbc4bbdc8799f5f89e42",
+    "url": "/inter-pro/static/css/main.a636f2e1.chunk.css"
+  },
+  {
+    "revision": "2fe40c6aab871a03e69b",
+    "url": "/inter-pro/static/js/2.4e5cd8f3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/inter-pro/static/js/2.4e5cd8f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc4bbdc8799f5f89e42",
+    "url": "/inter-pro/static/js/main.9cb88902.chunk.js"
+  },
+  {
+    "revision": "c623c74e708c456d9198",
+    "url": "/inter-pro/static/js/runtime-main.f4df1ff1.js"
+  },
+  {
+    "revision": "4dbac044a6cfa4762d63ae44ee27bc77",
+    "url": "/inter-pro/static/media/bg_img1.4dbac044.png"
+  },
+  {
+    "revision": "ec13b457a74f1d615db496c8e8a6c29c",
+    "url": "/inter-pro/static/media/bg_main.ec13b457.png"
+  },
+  {
+    "revision": "a95ec19194d73beffbdb5858395b4bba",
+    "url": "/inter-pro/static/media/bg_main1.a95ec191.png"
+  },
+  {
+    "revision": "31bdea5d598ba54697e024e5f14b64e0",
+    "url": "/inter-pro/static/media/exciting_img-11.31bdea5d.png"
+  },
+  {
+    "revision": "860c2add46f9ad697fe1c2ff0918c870",
+    "url": "/inter-pro/static/media/exciting_img-12.860c2add.png"
+  },
+  {
+    "revision": "f87cdbd7ad1526417a25ba45306e4fd5",
+    "url": "/inter-pro/static/media/footer_bg.f87cdbd7.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/inter-pro/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/inter-pro/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/inter-pro/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/inter-pro/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "2d8361e6610ecb32699c5cd7a0c2957b",
+    "url": "/inter-pro/static/media/supplier-pc-img.2d8361e6.png"
+  }
+]);
